4af64745a30f6de39e495a444c154afad7511a93
yaroslavkardanets <kardanets@gmail.com>
2022-08-07 16:23:22 +0300

    step 3. Реализация приветствия

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node;
 
-console.log("Welcome to the Brain Games!");
+import { greeting } from '../src/cli.js';
 
-import name from ../src/cls.js
+greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const name = readlineSync.question('Your answer: ');
+export const greeting = () => {
+  console.log("Welcome to the Brain Games!");
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
