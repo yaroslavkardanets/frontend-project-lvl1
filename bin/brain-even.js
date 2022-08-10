3ebb57e035d877c88befcda1c8f2727a87b1ab95
yaroslavkardanets <kardanets@gmail.com>
2022-08-10 10:51:37 +0300

    step 5. upd brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,4 +4,9 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
+const random = Math.random();
+const randomNumber = Math.round(random);
+
 console.log(`Hello, ${userName}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
