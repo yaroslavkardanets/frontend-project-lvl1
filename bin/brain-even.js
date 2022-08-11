e2252da2432884076a5dcfa41991234b68491f07
yaroslavkardanets <kardanets@gmail.com>
2022-08-11 15:13:51 +0300

    step 5. upd brain-even.js. Программа срабатывает один раз

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,9 +4,30 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 const userName = readlineSync.question('May I have your name? ');
-const random = Math.random();
-const randomNumber = Math.round(random);
-
 console.log(`Hello, ${userName}!`);
-
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const generateRandomNumber = Math.round(Math.random() * 100);
+console.log(`Question: ${generateRandomNumber}`);
+const userAnswer = readlineSync.question('Your answer: ');
+
+const checkEvenGame = () => {
+  const isEven = generateRandomNumber % 2 === 0;
+
+  if ((isEven && userAnswer === 'yes') || (!isEven && userAnswer === 'no')) {
+    console.log('Correct!');
+  } else {
+    console.log('Wrong answer!');
+  }
+};
+
+console.log(checkEvenGame());
+
+// if (readlineSync.keyInYN(`Question: ${randomNumber}`)) {
+//   // 'Y' key was pressed.
+//   console.log('Installing now...');
+//   // Do something...
+// } else {
+//   // Another key was pressed.
+//   console.log('Searching another...');
+//   // Do something...
+// }
