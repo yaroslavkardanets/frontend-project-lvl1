efa6d6bbdeab226ceff6e10ace32db9fca350a31
yaroslavkardanets <kardanets@gmail.com>
2022-08-11 15:55:48 +0300

    step 5. upd brain-even.js. Программа срабатывает один раз. Рефакторинг

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,28 @@
 #!/usr/bin/env node;
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const generateRandomNumber = Math.round(Math.random() * 100);
-console.log(`Question: ${generateRandomNumber}`);
-const userAnswer = readlineSync.question('Your answer: ');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+const gameRules = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+greeting();
+gameRules();
 
-const checkEvenGame = () => {
+const gameRound = () => {
+  const generateRandomNumber = Math.round(Math.random() * 100);
+  console.log(`Question: ${generateRandomNumber}`);
+  const userAnswer = readlineSync.question('Your answer: ');
   const isEven = generateRandomNumber % 2 === 0;
 
   if ((isEven && userAnswer === 'yes') || (!isEven && userAnswer === 'no')) {
     console.log('Correct!');
-  } else {
-    console.log('Wrong answer!');
   }
+  console.log('Wrong answer!');
 };
 
-console.log(checkEvenGame());
-
-// if (readlineSync.keyInYN(`Question: ${randomNumber}`)) {
-//   // 'Y' key was pressed.
-//   console.log('Installing now...');
-//   // Do something...
-// } else {
-//   // Another key was pressed.
-//   console.log('Searching another...');
-//   // Do something...
-// }
+gameRound();
