634a30d4f2c362bdbef80c84d6f0426c8d5ac275
yaroslavkardanets <kardanets@gmail.com>
2022-08-22 14:40:32 +0300

    step 7. upd brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node;
 import hello from '../src/index.js';
 import gcdGameRules from '../src/gcd-rules.js';
-import fullGame from '../games/calc.js';
+import fullGame from '../games/gcd.js';
 
+console.log('hello');
 hello();
+console.log('gcdGameRules');
 gcdGameRules();
+console.log('fullGame');
 fullGame();
