b3c30b92fefd7bfd3f84314f2a14d397c2a4eeac
yaroslavkardanets <kardanets@gmail.com>
2022-08-22 13:36:38 +0300

    step 7. upd tmp.js

--- bin/tmp.js
+++ bin/tmp.js
@@ -1,20 +1,18 @@
 const generateRandomNumber = () => Math.round(Math.random() * 100);
 
+const randomNumber1 = () => generateRandomNumber();
+// console.log(`число 1 = ${randomNumber1}`);
+const randomNumber2 = () => generateRandomNumber();
+// console.log(`число 2 = ${randomNumber2}`);
+
 const greatestDivisor = () => {
-  const randomNumber1 = generateRandomNumber();
-  console.log(`число 1 = ${randomNumber1}`);
-  const randomNumber2 = generateRandomNumber();
-  console.log(`число 2 = ${randomNumber2}`);
-  if (randomNumber1 === randomNumber2) {
-    return randomNumber1;
-  }
-  const biggestNumber = Math.max(randomNumber1, randomNumber2);
-  const smallestNumber = Math.min(randomNumber1, randomNumber2);
+  const biggestNumber = Math.max(randomNumber1(), randomNumber2());
+  const smallestNumber = Math.min(randomNumber1(), randomNumber2());
 
   for (let i = smallestNumber; i > 0; i -= 1) {
     if (smallestNumber % i === 0) {
       if (Number.isInteger(biggestNumber / i)) {
-        console.log('i = ', i);
+        // console.log('i = ', i);
         return i;
       }
     }
