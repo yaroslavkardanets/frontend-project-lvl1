86caec4687147e29e99292243b7d3bc54b6dedbb
yaroslavkardanets <kardanets@gmail.com>
2022-08-22 13:58:37 +0300

    step 7. upd tmp.js

--- bin/tmp.js
+++ bin/tmp.js
@@ -1,18 +1,21 @@
 const generateRandomNumber = () => Math.round(Math.random() * 100);
 
-const randomNumber1 = () => generateRandomNumber();
-console.log(`число 1 = ${randomNumber1()}`);
-const randomNumber2 = () => generateRandomNumber();
-console.log(`число 2 = ${randomNumber2()}`);
+const randomNumber1 = generateRandomNumber();
+console.log(`число 1 = ${randomNumber1}`);
+const randomNumber2 = generateRandomNumber();
+console.log(`число 2 = ${randomNumber2}`);
 
 const greatestDivisor = () => {
-  const biggestNumber = Math.max(randomNumber1(), randomNumber2());
-  const smallestNumber = Math.min(randomNumber1(), randomNumber2());
+  const biggestNumber = Math.max(randomNumber1, randomNumber2);
+  const smallestNumber = Math.min(randomNumber1, randomNumber2);
   console.log(`biggestNumber = ${biggestNumber}`);
   console.log(`smallestNumber = ${smallestNumber}`);
+  if (smallestNumber === 0 || smallestNumber === 1) {
+    return smallestNumber;
+  }
 
-  for (let i = smallestNumber; i > 0; i -= 1) {
-    if (smallestNumber % i === 0) {
+  for (let i = smallestNumber; i >= 0; i -= 1) {
+    if (smallestNumber % i === 0 /* && Number.isInteger(biggestNumber / i) */) {
       if (Number.isInteger(biggestNumber / i)) {
         console.log('i = ', i);
         return i;
@@ -21,4 +24,4 @@ const greatestDivisor = () => {
   }
 };
 console.log('Answer:');
-greatestDivisor();
+console.log(greatestDivisor());
