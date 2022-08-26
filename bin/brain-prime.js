3f32763b51c7da2cc879fd188c465ede50789f26
yaroslavkardanets <kardanets@gmail.com>
2022-08-26 12:54:54 +0300

    step 9. upd brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node;
 import hello from '../src/index.js';
 import primeGameRules from '../src/prime-rules.js';
-import fullGame from '../games/calc.js';
+import fullGame from '../games/prime.js';
 
 hello();
 primeGameRules();
