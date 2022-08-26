b3cdf3f456a5bf8616a2412c0cc095089ca76ddc
yaroslavkardanets <kardanets@gmail.com>
2022-08-26 10:54:30 +0300

    step 9. upd tmp.js функция определения простого числа

--- bin/tmp.js
+++ bin/tmp.js
@@ -1,37 +1,21 @@
 import readlineSync from 'readline-sync';
 
-import generateRandomRange from '../src/generate-random-range.js';
+import generateRandomNumber from '../src/generate-random-number.js';
 
-const gameRound = () => {
-  const range = [];
-  const rangeStart = generateRandomRange(0, 50);
-  console.log(`rangeStart = ${rangeStart}`);
-  const rangeSteps = generateRandomRange(5, 10);
-  console.log(`rangeSteps = ${rangeSteps}`);
-  const rangeStep = generateRandomRange(1, 10);
-  console.log(`rangeStep = ${rangeStep}`);
-  const rangeStop = rangeStart + (rangeStep * rangeSteps);
-  const missingNumber = generateRandomRange(0, rangeSteps);
-
-  for (let i = rangeStart; i < rangeStop; i += rangeStep) {
-    // console.log('i =', i);
-    range.push(i);
-  }
-  console.log(`range = ${range}`);
-  const getMissingNumber = range[missingNumber];
-  console.log(`getMissingNumber = ${getMissingNumber}`);
-  range[missingNumber] = '...';
-  console.log(`rangeMissing = ${range}`);
-  // return range;
-  console.log(`Question: ${range}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  if (`${getMissingNumber}` === `${userAnswer}`) {
-    console.log('Correct!');
+const randomNumber = generateRandomNumber();
+const isPrime = () => {
+  if (randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
     return true;
   }
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${getMissingNumber}'.`);
-  return false;
+  console.log(randomNumber);
+  const arr = [];
+  if (randomNumber % 2 !== 0) {
+    for (let i = 3; i < randomNumber / 2; i += 2) {
+      if (Number.isInteger(randomNumber / i)) {
+        arr.push(i);
+        console.log(arr);
+      }
+    }
+  }
 };
-
-gameRound();
+isPrime();
