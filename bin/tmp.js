1588a606e6523377049eca04dbb58620eca97647
yaroslavkardanets <kardanets@gmail.com>
2022-08-27 16:27:08 +0300

    step 9. upd tmp.js

--- bin/tmp.js
+++ bin/tmp.js
@@ -3,19 +3,26 @@ import readlineSync from 'readline-sync';
 import generateRandomNumber from '../src/generate-random-number.js';
 
 const randomNumber = generateRandomNumber();
+
 const isPrime = () => {
+  console.log(randomNumber);
   if (randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
     return true;
-  }
-  console.log(randomNumber);
-  const arr = [];
-  if (randomNumber % 2 !== 0) {
+  } if (randomNumber % 2 === 0) {
+    return false;
+  } if (randomNumber % 2 !== 0) {
+    const arr = [];
     for (let i = 3; i < randomNumber / 2; i += 2) {
       if (Number.isInteger(randomNumber / i)) {
         arr.push(i);
         console.log(arr);
       }
     }
+    if (arr.length > 0) {
+      return false;
+    }
+    return true;
   }
 };
+console.log(isPrime());
 isPrime();
