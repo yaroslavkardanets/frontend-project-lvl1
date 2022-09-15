b2cf6c66934726af77280a5ba81dbb6afe6000e9
kardanets@gmail.com <kardanets@gmail.com>
2022-09-15 13:59:18 +0300

    refactoring 2. upd prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,53 +5,33 @@ import generateRandomRange from '../generate-random-range.js';
 
 export const primeGameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (num) => {
+  if (num === 0 || num === 1 || num === 2) {
+    return 'yes';
+  } if (num % 2 === 0) {
+    return 'no';
+  }
+  const arr = [];
+  for (let i = 3; i < num / 2; i += 2) {
+    if (Number.isInteger(num / i)) {
+      arr.push(i);
+    }
+  }
+  if (arr.length > 0) {
+    return 'no';
+  }
+  return 'yes';
+};
+
 export const primeGameRound = () => {
   const questionCondition = [];
   const randomNumber = generateRandomRange();
 
-  const isPrime = () => {
-    if (randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
-      return 'yes';
-    } if (randomNumber % 2 === 0) {
-      return 'no';
-    } if (randomNumber % 2 !== 0) {
-      const arr = [];
-      for (let i = 3; i < randomNumber / 2; i += 2) {
-        if (Number.isInteger(randomNumber / i)) {
-          arr.push(i);
-        }
-      }
-      if (arr.length > 0) {
-        return 'no';
-      }
-      return 'yes';
-    }
-  };
-  // console.log('*** isPrime: ', isPrime());
+  // console.log('*** isPrime: ', isPrime(randomNumber));
 
   const question = `Question: ${randomNumber}`;
-  questionCondition.push(question, isPrime());
+  questionCondition.push(question, isPrime(randomNumber));
+  // console.log('*** questionCondition: ', questionCondition);
   return questionCondition;
   // const userAnswer = readlineSync.question('Your answer: ');
-
-  // if ((isPrime() && userAnswer === 'yes') || (!isPrime() && userAnswer === 'no')) {
-  //   console.log('Correct!');
-  //   return true;
-  // }
-  // console.log('Wrong answer!');
-  // return false;
 };
-
-// Надо подумать, как этот модуль будет работать с разными играми
-// export const fullGame = () => {
-//   for (let i = 0; i < 3; i += 1) {
-//     const game = gameRound();
-//     if (!game) {
-//       console.log(`Let's try again, ${userName}!`);
-//       return;
-//     }
-//   }
-//   console.log(`Congratulations, ${userName}!`);
-// };
-
-// export default fullGame;
