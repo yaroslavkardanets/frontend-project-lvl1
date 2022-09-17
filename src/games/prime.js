6298956f7d3f130234bbbe0a48cfa6fc10315f11
yaroslavkardanets <kardanets@gmail.com>
2022-09-17 21:11:35 +0300

    refactoring3. upd prime.js fix isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,10 @@ import generateRandomRange from '../generate-random-range.js';
 const primeGameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num === 0 || num === 1 || num === 2) {
-    return 'yes';
+  if (num >= 0 && num <= 2) {
+    return true;
   } if (num % 2 === 0) {
-    return 'no';
+    return false;
   }
   const arr = [];
   for (let i = 3; i < num / 2; i += 2) {
@@ -16,16 +16,16 @@ const isPrime = (num) => {
     }
   }
   if (arr.length > 0) {
-    return 'no';
+    return false;
   }
-  return 'yes';
+  return true;
 };
 
 const primeGameRound = () => {
   const randomNumber = generateRandomRange();
-  // const question = `Question: ${randomNumber}`;
+  const prime = isPrime(randomNumber) ? 'yes' : 'no';
 
-  return [`${randomNumber}`, isPrime(randomNumber)];
+  return [`${randomNumber}`, prime];
 };
 
 export default () => runGame(primeGameRules, primeGameRound);
