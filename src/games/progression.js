6eb75b11c91376a87e648bafbc8ad268241d1558
yaroslavkardanets <kardanets@gmail.com>
2022-09-17 20:13:29 +0300

    refactoring3. upd progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,25 +3,38 @@ import generateRandomRange from '../generate-random-range.js';
 
 const progressionGameRules = 'What number is missing in the progression?';
 
-const progressionGameRound = () => {
-  const questionCondition = [];
+const getProgression = (start, long, step) => {
   const range = [];
+  for (let i = start; i < long; i += step) {
+    range.push(i);
+  }
+  // console.log(range);
+  return range;
+};
+
+const progressionGameRound = () => {
+  // const questionCondition = [];
+  // const progression = [];
   const rangeStart = generateRandomRange(0, 50);
   const rangeStepsAmmount = generateRandomRange(5, 10);
   const rangeStepInterval = generateRandomRange(1, 10);
-  const rangeStop = rangeStart + (rangeStepInterval * rangeStepsAmmount);
-  const missingNumber = generateRandomRange(0, rangeStepsAmmount);
+  const rangeStop = rangeStart + (rangeStepInterval * (rangeStepsAmmount));
+  const missingNumber = generateRandomRange(0, rangeStepsAmmount - 1);
+  // console.log('** rangeStart: ', rangeStart);
+  // console.log('*** rangeStepsAmmount: ', rangeStepsAmmount);
+  // console.log('*** rangeStepInterval: ', rangeStepInterval);
+  // console.log('*** rangeStop: ', rangeStop);
+  // console.log('*** missingNumber: ', missingNumber);
 
-  for (let i = rangeStart; i <= rangeStop; i += rangeStepInterval) {
-    range.push(i);
-  }
+  const progression = getProgression(rangeStart, rangeStop, rangeStepInterval);
+  // console.log('*** progression: ', progression);
 
-  const getMissingNumber = range[missingNumber];
+  const getMissingNumber = progression[missingNumber];
 
-  range[missingNumber] = '..';
-  const question = `Question: ${range.join(' ')}`;
-  questionCondition.push(question, `${getMissingNumber}`);
-  return questionCondition;
+  progression[missingNumber] = '..';
+  const question = `Question: ${progression.join(' ')}`;
+  // questionCondition.push(question, `${getMissingNumber}`);
+  return [question, `${getMissingNumber}`];
 };
 
 export default () => runGame(progressionGameRules, progressionGameRound);
