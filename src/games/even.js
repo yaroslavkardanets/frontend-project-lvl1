a341c27b154a1903176c8573e85d3ea538647210
yaroslavkardanets <kardanets@gmail.com>
2022-09-17 10:35:52 +0300

    refactoring3. upd even.js

--- src/games/even.js
+++ src/games/even.js
@@ -5,13 +5,11 @@ const evenGameRules = 'Answer "yes" if the number is even, otherwise answer "no"
 const isEven = (num) => num % 2 === 0;
 
 const evenGameRound = () => {
-  const questionCondition = [];
   const randomNumber = generateRandomRange();
   const question = `Question: ${randomNumber}`;
-  questionCondition.push(question);
   const conditionOfUserAnswer = isEven(randomNumber) ? 'yes' : 'no';
-  questionCondition.push(conditionOfUserAnswer);
-  return questionCondition;
+
+  return [question, conditionOfUserAnswer];
 };
 
 export default () => runGame(evenGameRules, evenGameRound);
