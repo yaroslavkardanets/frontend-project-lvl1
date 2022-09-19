61b779e20214593dfcf6a5084b08ff14794179f5
yaroslavkardanets <kardanets@gmail.com>
2022-09-19 16:43:20 +0300

    refactoring5. upd calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,9 +4,9 @@ import generateRandomNumber from '../generate-random-number.js';
 const calcGameRules = 'What is the result of the expression?';
 const calculate = (num1, num2, operator) => {
   switch (operator) {
-    case 0:
+    case '-':
       return num1 - num2;
-    case 1:
+    case '+':
       return num1 + num2;
     default:
       return num1 * num2;
@@ -18,7 +18,7 @@ const calcGameRound = () => {
   const randomNumberTwo = generateRandomNumber();
   const operators = ['-', '+', '*'];
   const randomOperatorIndex = generateRandomNumber(0, operators.length - 1);
-  const rightAnswer = calculate(randomNumberOne, randomNumberTwo, randomOperatorIndex);
+  const rightAnswer = calculate(randomNumberOne, randomNumberTwo, operators[randomOperatorIndex]);
 
   return [`${randomNumberOne} ${operators[randomOperatorIndex]} ${randomNumberTwo}`, `${rightAnswer}`];
 };
